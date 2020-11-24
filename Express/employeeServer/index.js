e297f06e771213d7f5d6a2b04ebe751a17da4c62
Marianna Mori <marianna.mori1@gmail.com>
2020-11-24 12:47:23 +0200

    updates tue class

--- Express/employeeServer/index.js
+++ Express/employeeServer/index.js
@@ -36,7 +36,7 @@ app.get('/getPerson', (req,res)=>
 res.render('getPerson',{
     title:'Get',
     header:'Get',
-    action: 'getPerson'
+    action: '/getPerson'
 })
 );
 
@@ -47,12 +47,100 @@ app.post('/getPerson', (req,res)=>{
     dataStorage.get(personId)
     .then(employee=>res.render('personPage',{result:createPerson(employee)}))
     .catch(error=>sendErrorPage(res,error));
+});
+
+
+app.get('/inputform', (req,res)=>
+res.render('form',{
+    title:'Add person',
+    header:'Add a new person',
+    action:'/insert',
+    personId:{value:'', readonly:''},
+    firstname:{value:'', readonly:''},
+    lastname:{value:'', readonly:''},
+    department:{value:'', readonly:''},
+    salary:{value:'', readonly:''}
 })
+);
+
+app.post('/insert', (req, res)=>{
+    if(!req.body) res.sendStatus(500);
+
+    dataStorage.insert(createEmployee(req.body))
+    .then(status => sendStatusPage(res,status))
+    .catch(error => sendErrorPage(res,error));
+});
+
+app.get('/updateform', (req,res)=>
+res.render('form',{
+    title:'Update person',
+    header: 'Update person data',
+    action:'/updatedata',
+    personId:{value:'', readonly:''},
+    firstname:{value:'', readonly:'readonly'},
+    lastname:{value:'', readonly:'readonly'},
+    department:{value:'', readonly:'readonly'},
+    salary:{value:'', readonly:'readonly'}
+})
+
+);
+app.post('/updatedata', async (req,res)=>{
+    if(!req.body){
+        res.sendStatus(500);
+    } 
+    else {
+        try {
+            const personId=req.body.personId;
+            const employee = await dataStorage.get(personId);
+            const person = createPerson(employee);
+            res.render('form', {
+                title:'Update person',
+                header:'Update person data',
+                action:'/updateperson',
+                personId:{value:employee.employeeId, readonly:'readonly'},
+                firstname:{value:person.firstname, readonly:''},
+                lastname:{value:person.lastname, readonly:''},
+                department:{value:person.department, readonly:''},
+                salary:{value:person.salary, readonly:''}
+            });
+        }
+        catch(error){
+            sendErrorPage(res,error);
+        }
+    }
+});
+
+app.post('/updateperson',(req,res)=>{
+    if(!req.body) res.sendStatus(500);
+  else  dataStorage.update(createEmployee(req.body))
+    .then(status=>sendStatusPage(res,status))
+    .catch(error=>sendErrorPage(res,error));
+});
+
+app.get('/removeperson', (req,res)=>
+res.render('getPerson',{
+    title:'Remove',
+    header:'Remove a person',
+    action:'/removeperson'
+})
+);
+
+app.port('/removeperson', (req,res)=>{
+    if(!req.body) res.sendStatus(500);
+    const personId=req.body.personId
+    dataStorage.remove(personId)
+    .then(status=>sendStatusPage(res,status))
+    .catch(error=>sendErrorPage(res,error));
+});
 
 server.listen(port, host, ()=>console.log(`Server ${host}:${port} running`));
 
-function sendErrorPage(res,error) {
-    res.end();
+function sendErrorPage(res,error, title='Error', header='Error') {
+    sendStatusPage(res, error, title, header);
+}
+
+function sendStatusPage(res, status, title='Status', header='Status'){
+    return res.render('statusPage', {title,header,status});
 }
 
 //from employee to person
