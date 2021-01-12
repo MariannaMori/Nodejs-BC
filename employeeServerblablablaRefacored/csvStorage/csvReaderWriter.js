93271b298476a5dcbc999f9f60016b12a3faa727
Marianna Mori <marianna.mori1@gmail.com>
2021-01-12 12:26:46 +0200

    recap

---
+++ employeeServerblablablaRefacored/csvStorage/csvReaderWriter.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs=require('fs');
+const readline = require('readline');
+
+async function readStorage(storageFile){
+    const linereader=readline.createInterface({
+        input: fs.createReadStream(storageFile)
+    });
+    const lines=[];
+    try{
+        for await (const line of linereader) {
+            lines.push(line);
+        }
+        const data=[];
+        for(let line of lines){
+            data.push(line.split('","').map(d=>d.replace(/["]/g,'')));
+        }
+    //    console.log(data);
+        return csvToJson(data);
+    }
+    catch(err) {
+        return [];
+    }
+}
+
+async function writeStorage(storageFile,data){
+    try{
+        await fs.promises.writeFile(storageFile, createCsv(data),
+        {encoding:'utf8', flag:'w'});
+        return true;
+
+    }
+    catch(err) {
+        return false;
+    }
+}
+
+function csvToJson(data) {
+    const [headers, ...datalines] = data;
+    const jsonData = [];
+    for (let employeeLine of datalines) {
+        if (employeeLine.length === headers.length) {
+            const employee = {};
+            for (let i = 0; i < headers.length; i++) {
+                employee[headers[i]] = employeeLine[i];
+            }
+            jsonData.push(employee);
+        }
+    }
+    return jsonData;
+}
+
+function createCsv(jsondata){
+    let csvString='';
+    if(jsondata.length>0) {
+        csvString+='"'+Object.keys(jsondata[0]).join('","')+'"\n';
+        for(let employee of jsondata) {
+            csvString+='"'+Object.values(employee).join('","')+'"\n';
+        }
+    }
+    return csvString;
+}
+
+
+module.exports = {
+    readStorage, writeStorage
+}
---
+++ employeeServerblablablaRefacored/libraries/dataStorageLayer (2).js
@@ -0,0 +1,102 @@
+'use strict';
+
+const path = require('path');
+
+//wrapper function
+function createDataStorage(baseDir, config){
+    const libPath = path.join(baseDir, config.storageLibraries.folder);
+    const {CODES,MESSAGES}=require(path.join(libPath,config.storageLibraries.errorCodes));
+    const { initLayerFunctions } = require(path.join(libPath, config.storageLibraries.layerFunctions));
+
+    const {
+        getAllFromStorage, 
+        getFromStorage, 
+        addToStorage, 
+        removeFromStorage, 
+        updateStorage
+    } = initLayerFunctions(baseDir, config.storage);
+
+    class Datastorage{
+        get CODES() {
+            return CODES;
+        }
+
+        getAll() {
+            return getAllFromStorage();
+        }
+
+        get(id){
+            return new Promise(async (resolve,reject) =>{
+                if(!id) {
+                    reject(MESSAGES.NOT_FOUND('<empty id>'));
+                }
+                else {
+                    const result = await getFromStorage(id);
+                    if(result) {
+                        resolve(result);
+                    }
+                    else{
+                        reject(MESSAGES.NOT_FOUND(id));
+                    }
+                }
+            });
+        }
+        insert(employee){
+            return new Promise(async (resolve,reject)=>{
+                if(!(employee && employee.employeeId &&
+                     employee.firstname && employee.lastname)){
+                         reject(MESSAGES.NOT_INSERTED());
+                }
+                else{
+                    if( await addToStorage(employee)) {
+                        resolve(MESSAGES.INSERT_OK(employee.employeeId));
+                    }
+                    else {
+                        reject(MESSAGES.ALREADY_IN_USE(employee.employeeId));
+                    }
+                }
+            });
+        }
+
+        remove(employeeId){
+            return new Promise(async (resolve, reject)=>{
+                if(!employeeId) {
+                    reject(MESSAGES.NOT_FOUND('<empty>'));
+                }
+                else {
+                    if(await removeFromStorage(employeeId)) {
+                        resolve(MESSAGES.REMOVE_OK(employeeId));
+                    }
+                    else {
+                        reject(MESSAGES.NOT_REMOVED());
+                    }
+                }
+            });
+        }
+
+        update(employee) {
+            return new Promise( async (resolve, reject)=>{
+                if(!(employee && employee.employeeId &&
+                     employee.firstname && employee.lastname)){
+                         reject(MESSAGES.NOT_UPDATED());
+                }
+                else {
+                    if(await updateStorage(employee)){
+                        resolve(MESSAGES.UPDATE_OK(employee.employeeId));
+                    }
+                    else {
+                        reject(MESSAGES.NOT_UPDATED());
+                    }
+                }
+            });
+        }
+
+    } // class end
+
+    return new Datastorage();
+
+} //wrapper end
+
+module.exports = {
+    createDataStorage
+}
