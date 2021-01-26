4c6d31d463feec514fab926e65b252f81f0c509c
Marianna Mori <marianna.mori1@gmail.com>
2021-01-26 12:31:05 +0200

    class 26.1 vantaa jobs

---
+++ vantaa/index.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const data = require('./vantaaData.json');
+
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const host='localhost';
+
+const server=http.createServer(app);
+
+app.get('/', (req,res)=> res.sendFile(path.join(__dirname, 'vantaaJobsHome.html')));
+app.get('/json', (req,res)=>res.json(data));
+server.listen(port,host, ()=>console.log(`Server ${host} listens port ${port}`));
