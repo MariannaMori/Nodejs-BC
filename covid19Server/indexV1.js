658e50165588bcd81bd342620b02e3caa4860687
Marianna Mori <marianna.mori1@gmail.com>
2021-01-26 19:07:53 +0200

    part 1 lesson 26.1

---
+++ covid19Server/indexV1.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const covidData = require('./covid19DataFIN.json');
+
+const {port,host} = require('./config.json');
+
+const app = express();
+const server = http.createServer(app);
+
+const homePath=path.join(__dirname,'home.html');
+
+app.use(cors());
+
+app.get('/', (req,res)=>res.sendFile(homePath));
+
+app.get('/api/v1/data', async(req,res)=>{
+    const dateStrings=Object.keys(covidData.result);
+    const data=dateStrings.map(date=>covidData.result[date].confirmed);
+   
+    res.json(data);
+})
+
