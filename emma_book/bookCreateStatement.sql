85b9163957512608e0b448fd9f52014fc2d6f57d
Marianna Mori <marianna.mori1@gmail.com>
2021-03-02 11:00:40 +0200

    class 2.3

---
+++ emma_book/bookCreateStatement.sql
@@ -0,0 +1,22 @@
+drop database if exists bookdb;
+create database bookdb;
+
+
+
+create table bookdb.book(
+    bookID integer not null primary key,
+    name varchar(62) not null,
+    author varchar(12) not null,
+    type varchar(26) not null,
+    year integer not null
+
+);
+
+drop user if exists 'adrian'@'localhost';
+create user 'adrian'@'localhost' identified by 'tN5LKSaK';
+
+grant all privileges on bookdb.* to 'adrian'@'localhost';
+
+insert into bookdb.book values(1, 'SQL-mysteries', 'Matt Wilson', 'pocketbook' , 1990 );
+insert into bookdb.book values(2, 'Fairytales', 'Mo Austin', 'pocketbook' , 1998 );
+
---
+++ emma_book/test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const Datastorage = require('./dataStorageLayer');
+
+const db = new Datastorage();
+
+db.getAll().then(console.log).catch(console.log);
+db.get(1).then(console.log).catch(console.log);
+
+db.get(100).then(console.log).catch(console.log); //doesnt exist, just to test error
